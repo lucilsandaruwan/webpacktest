544be0f420ceeab54f6559d0a2142f92a967a1bf
Lucil Sandaruwan <lucil.sandaruwan@lazada.com>
2018-09-28 12:08:40 +0800

    fix: fixed an issue of unit test

--- tests/operations.js
+++ tests/operations.js
@@ -2,5 +2,5 @@ const assert = require('assert');
 const operations = require('../modules/operations')
 
 it('correctly calculates the sum of 1 and 3', () => {
-  assert.equal(operations.add(1, 3), 5);
+  assert.equal(operations.add(1, 3), 4);
 });
